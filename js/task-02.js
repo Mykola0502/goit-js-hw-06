ae5b9c8469ba572fd34ebf98a682ff154de6d07d
Mykola Saliuk <haker0502@ukr.net>
2022-10-18 00:48:54 +0300

    додає завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients');
+
+const makeIngradients = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+
+    return itemEl;
+  });
+};
+
+const items = makeIngradients(ingredients);
+ingredientsContainerEl.append(...items);
