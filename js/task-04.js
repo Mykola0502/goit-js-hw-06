95c18fd515e74db82a74917afb9b08b91ac4b7f1
Mykola Saliuk <haker0502@ukr.net>
2022-10-19 00:39:35 +0300

    Вносить правки у завдання 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,27 @@
+// const counter = {
+//   counterValue: 0,
+//   increment() {
+//     this.counterValue += 1;
+//   },
+//   decrement() {
+//     this.counterValue -= 1;
+//   },
+// };
+
+// const decrementBtn = document.querySelector('[data-action="decrement"]');
+// const incrementBtn = document.querySelector('[data-action="increment"]');
+// const counterValueEl = document.querySelector('#value');
+
+// decrementBtn.addEventListener('click', () => {
+//   counter.decrement();
+//   counterValueEl.textContent = counter.counterValue;
+// });
+
+// incrementBtn.addEventListener('click', () => {
+//   counter.increment();
+//   counterValueEl.textContent = counter.counterValue;
+// });
+
 const counter = {
   counterValue: 0,
   increment() {
@@ -14,6 +38,9 @@ const counterValueEl = document.querySelector('#value');
 
 decrementBtn.addEventListener('click', () => {
   counter.decrement();
+  if (counter.counterValue < 0) {
+    counter.counterValue = 0;
+  }
   counterValueEl.textContent = counter.counterValue;
 });
 
