ea172e9e62f43657451a6f84e3fe275caf0af945
Mykola Saliuk <haker0502@ukr.net>
2022-10-19 00:13:26 +0300

    додає код 6 завдання

---
+++ js/task-06.js
@@ -0,0 +1,39 @@
+// const textInput = document.querySelector('#validation-input');
+
+// textInput.addEventListener('blur', event => {
+//   if (
+//     event.currentTarget.value.length == textInput.getAttribute('data-length')
+//   ) {
+//     console.log('Правильно');
+//     textInput.classList.remove('invalid');
+//     textInput.classList.add('valid');
+//     console.log(textInput.classList);
+//     return;
+//   } else {
+//     console.log('Не правильно');
+//     textInput.classList.remove('valid');
+//     textInput.classList.add('invalid');
+//     console.log(textInput.classList);
+//   }
+// });
+
+const textInput = document.querySelector('#validation-input');
+
+textInput.addEventListener('blur', event => {
+  if (!event.currentTarget.value) {
+    textInput.classList.remove('invalid', 'valid');
+    console.log(textInput.classList);
+  } else if (
+    event.currentTarget.value.length == textInput.getAttribute('data-length')
+  ) {
+    console.log('Правильно');
+    textInput.classList.remove('invalid');
+    textInput.classList.add('valid');
+    console.log(textInput.classList);
+  } else {
+    console.log('Не правильно');
+    textInput.classList.remove('valid');
+    textInput.classList.add('invalid');
+    console.log(textInput.classList);
+  }
+});
