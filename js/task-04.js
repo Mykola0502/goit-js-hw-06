64915ccec8b027205e6f7739ecc1267aa290494b
Mykola Saliuk <haker0502@ukr.net>
2022-10-18 20:53:43 +0300

    додає код 4 завдання

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+const counter = {
+  counterValue: 0,
+  increment() {
+    this.counterValue += 1;
+  },
+  decrement() {
+    this.counterValue -= 1;
+  },
+};
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const counterValueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', () => {
+  counter.decrement();
+  counterValueEl.textContent = counter.counterValue;
+});
+
+incrementBtn.addEventListener('click', () => {
+  counter.increment();
+  counterValueEl.textContent = counter.counterValue;
+});
