d85a8803866f3ed5d32931fb90a71cd352727314
Mykola Saliuk <haker0502@ukr.net>
2022-10-20 01:31:39 +0300

    додає код завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
+const user = {};
+console.log(user);
 
+formEl.setAttribute('autocomplete', 'off');
+
+formEl.addEventListener('submit', submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Будь ласка заповніть всі поля!');
+  }
+  user.email = email.value;
+  user.password = password.value;
+  console.log(user);
+  event.currentTarget.reset();
+}
