92afcd802b1fb51b3924d8bd8d4732757a19fb50
Mykola Saliuk <haker0502@ukr.net>
2022-10-20 23:49:27 +0300

    додає код 9 завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const btnChangeColorEl = document.querySelector('.change-color');
+const outputTextEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onBtnChangeColorElClick = () => {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  outputTextEl.textContent = `${getRandomHexColor()}`;
+};
+
+btnChangeColorEl.addEventListener('click', onBtnChangeColorElClick);
