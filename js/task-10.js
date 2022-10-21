6d307909a04436b83dc086934cf7c082703e6954
Mykola Saliuk <haker0502@ukr.net>
2022-10-21 10:09:07 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
+// const inputChangeEl = document.querySelector('input[type="number"]');
+// const btnCreateEl = document.querySelector('button[data-create]');
+// const btnDestroyEl = document.querySelector('button[data-destroy]');
+// const divBoxesEl = document.querySelector('#boxes');
+// let amount;
+// let divSize = 30;
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const onInputChange = event => (amount = event.currentTarget.value);
+
+// const createBoxes = () => {
+//   for (let i = 1; i <= amount; i += 1) {
+//     const divMarkup = `<div style="background-color:${getRandomHexColor()}; width:${divSize}px; height:${divSize}px;"></div>`;
+//     divSize += 10;
+//     divBoxesEl.insertAdjacentHTML('beforeend', divMarkup);
+//   }
+// };
+
+// const destroyBoxes = () => {
+//   divBoxesEl.innerHTML = '';
+//   divSize = 30;
+// };
+
+// inputChangeEl.addEventListener('input', onInputChange);
+// btnCreateEl.addEventListener('click', createBoxes);
+// btnDestroyEl.addEventListener('click', destroyBoxes);
+
 const inputChangeEl = document.querySelector('input[type="number"]');
 const btnCreateEl = document.querySelector('button[data-create]');
 const btnDestroyEl = document.querySelector('button[data-destroy]');
@@ -11,12 +41,16 @@ function getRandomHexColor() {
 
 const onInputChange = event => (amount = event.currentTarget.value);
 
+let divMarkup = '';
+
 const createBoxes = () => {
   for (let i = 1; i <= amount; i += 1) {
-    const divMarkup = `<div style="background-color:${getRandomHexColor()}; width:${divSize}px; height:${divSize}px;"></div>`;
+    divMarkup += `<div style="background-color:${getRandomHexColor()}; width:${divSize}px; height:${divSize}px;"></div>`;
     divSize += 10;
-    divBoxesEl.insertAdjacentHTML('beforeend', divMarkup);
   }
+
+  divBoxesEl.insertAdjacentHTML('beforeend', divMarkup);
+  divMarkup = '';
 };
 
 const destroyBoxes = () => {
