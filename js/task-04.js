56a70d0b972c328bfc5ade1f55bace5adf85c545
Mykola Saliuk <107276054+Mykola0502@users.noreply.github.com>
2024-07-13 22:26:15 +0300

    Update task-04.js
    add disabled btn

--- js/task-04.js
+++ js/task-04.js
@@ -24,27 +24,49 @@
 
 const counter = {
   counterValue: 0,
-  increment() {
-    this.counterValue += 1;
-  },
   decrement() {
     this.counterValue -= 1;
+    if (this.counterValue === 0) {
+      decrementBtn.disabled = true;
+    }
+  },
+  increment() {
+    this.counterValue += 1;
+    if (this.counterValue > 0) {
+      decrementBtn.disabled = false;
+    }
   },
 };
-
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
-const counterValueEl = document.querySelector('#value');
+const counterValueEl = document.querySelector("#value");
 
-decrementBtn.addEventListener('click', () => {
-  counter.decrement();
-  if (counter.counterValue < 0) {
+decrementBtn.addEventListener("click", onDecrement);
+incrementBtn.addEventListener("click", onIncrement);
+
+if (counter.counterValue === 0) {
+  decrementBtn.disabled = true;
+}
+
+function onDecrement() {
+  // if (counter.counterValue <= 1) {
+  //   decrementBtn.disabled = true;
+  // }
+  if (counter.counterValue > 0) {
+    counter.decrement();
+  } else {
     counter.counterValue = 0;
   }
-  counterValueEl.textContent = counter.counterValue;
-});
 
-incrementBtn.addEventListener('click', () => {
-  counter.increment();
   counterValueEl.textContent = counter.counterValue;
-});
+}
+
+function onIncrement() {
+  counter.increment();
+
+  // if (counter.counterValue > 0) {
+  //   decrementBtn.disabled = false;
+  // }
+
+  counterValueEl.textContent = counter.counterValue;
+}
